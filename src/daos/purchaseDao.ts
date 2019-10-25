81b26c955c9b32b5b274146ea3bd5f9933f253da
Oliver Lewis <oliverlewis@uvic.ca>
2019-10-25 14:00:19 -0700

    sprint1

--- src/daos/purchaseDao.ts
+++ src/daos/purchaseDao.ts
@@ -8,12 +8,11 @@ export class PurchaseDao {
     // }
 
     // Gets all purchases related to the userID
-
     public async getUsersPurchases(userID : any) {
 
         try {
             let database = await DbClient.connect();
-            return database!.collection(this.purchases_collection).find({"userID" : userID}).toArray();
+            return database!.collection(this.purchasesCollection).find({"userID" : userID}).toArray();
         } catch {
             // console.log("Dao: Error getting purchases for user");
         }
@@ -27,7 +26,7 @@ export class PurchaseDao {
         try {
 
             let database = await DbClient.connect();
-            return database!.collection(this.purchases_collection).find().toArray();
+            return database!.collection(this.purchasesCollection).find().toArray();
         } catch {
             // console.log("Dao: Error getting all user purchases");
         }
