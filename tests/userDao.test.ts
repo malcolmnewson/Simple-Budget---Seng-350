4a1691a6ed5d9aacb816bf801ef41d976a323b2f
Oliver Lewis <oliverlewis@uvic.ca>
2019-10-25 07:24:13 -0700

    Added userDao test framework

---
+++ tests/userDao.test.ts
@@ -0,0 +1,11 @@
+import {UserDao} from "../src/daos/userDao"
+
+/*test('get user', () => {
+   userDao : new UserDao();
+   expect(userDao.getUser("oliverlewis"))
+});*/
+
+/*
+test('sample', () => {
+    expect(sum(1,2)).toBe(3);
+});*/
