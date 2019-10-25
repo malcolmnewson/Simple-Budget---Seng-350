931adbcf0d1248b502d214bc640fb0e294c588e1
Oliver Lewis <oliverlewis@uvic.ca>
2019-10-25 09:33:02 -0700

    Fixed indenting

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 // jest.config.js
 
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: [
-      "<rootDir>/src/",
-      "<rootDir>/tests/"
-  ]
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: [
+        "<rootDir>/src/",
+        "<rootDir>/tests/"
+    ]
 };
