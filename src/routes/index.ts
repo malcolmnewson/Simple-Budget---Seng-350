c9c52e79914230661f836cf61367b20643360186
Emily <emily.sluis@gmail.com>
2019-10-25 17:08:48 -0700

    Added admin page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -21,13 +21,14 @@ export class IndexRoute extends BaseRoute {
         //log
         console.log("[IndexRoute::create] Creating index route.");
 
+
         //add home page route
         router.get("/", async (req: Request, res: Response, next: NextFunction) => {
             new IndexRoute().index(req, res, next);
         });
 
         router.get("/user/:userID", async (req: Request, res: Response, next: NextFunction) => {
-            new IndexRoute().purchases(req, res, next);
+            new IndexRoute().login(req, res, next);
         });
     }
 
@@ -42,7 +43,7 @@ export class IndexRoute extends BaseRoute {
     }
 
 
-    private async requestUsers(req: Request, res: Response, next: NextFunction) {
+    private async requestAllUsers(req: Request, res: Response, next: NextFunction) {
         let data = "";
         let apiResponse = await new Promise((resolve, reject) => {
             http.get("http://localhost:3000/users", async (res) => {
@@ -50,7 +51,6 @@ export class IndexRoute extends BaseRoute {
                     data += chunk;
                 });
                 await res.on("end", () => {
-                    //console.log("***data: "+ data);
                     apiResponse = JSON.parse(data);
                     resolve(apiResponse);
                 })
@@ -59,9 +59,26 @@ export class IndexRoute extends BaseRoute {
 
         const userIDs = [];
         for (let user of (apiResponse as any).users) {
-            userIDs.push(user.userID);
+            userIDs.push(user);
         }
         return userIDs;
+    }
+
+    private async requestUser(userID: String, req: Request, res: Response, next: NextFunction) {
+        let address = "http://localhost:3000/users/" + userID;
+        let data = "";
+        let apiResponse = await new Promise((resolve, reject) => {
+            http.get(address, async (res) => {
+                res.on("data", chunk => {
+                    data += chunk;
+                });
+                await res.on("end", () => {
+                    apiResponse = JSON.parse(data);
+                    resolve(apiResponse);
+                })
+            });
+        });
+        return (apiResponse as any).user;
     }
 
     private async requestPurchases(userID: String, req: Request, res: Response, next: NextFunction) {
@@ -94,13 +111,18 @@ export class IndexRoute extends BaseRoute {
      * @method index
      * @param req {Request} The express Request object.
      * @param res {Response} The express Response object.
+     * @param next
      * @next {NextFunction} Execute the next method.
      */
     public async index(req: Request, res: Response, next: NextFunction) {
         //set custom title
         this.title = "Budget App | Welcome!";
 
-        let userIDs = await this.requestUsers(req, res, next);
+        let userData = await this.requestAllUsers(req, res, next);
+        let userIDs = [];
+        for (let user of userData) {
+            userIDs.push(user.userID);
+        }
 
         //set options
         let options: Object = {
@@ -110,8 +132,22 @@ export class IndexRoute extends BaseRoute {
 
         //render template
         this.render(req, res, "index", options);
+    }
 
+    private async login(req: Request, res: Response, next: NextFunction) {
+        let userID = req.params.userID;
+        let userData = await this.requestUser(userID, req, res, next);
 
+        if (typeof userData === 'undefined') {
+            console.log("NOT A USER");
+            res.status(404).send("Sorry user doesn't exist!");
+        } else if (userData.admin) {
+            console.log("THIS IS ADMIN");
+            this.admin(req, res, next);
+        } else {
+            console.log("REGULAR USER");
+            this.purchases(req, res, next);
+        }
     }
 
     private async purchases(req: Request, res: Response, next: NextFunction) {
@@ -120,12 +156,30 @@ export class IndexRoute extends BaseRoute {
         let userID = req.params.userID;
         let purchases = await this.requestPurchases(userID, req, res, next);
 
+        let categories = purchases.map(item => item.category)
+            .filter((value, index, self) => self.indexOf(value) === index);
+        categories.sort();
+
+        purchases.sort((a, b) => (a.date > b.date) ? -1 : 1);
+
         let options: Object = {
             user: userID,
+            categories: categories,
             purchases: purchases
         };
-
         this.render(req, res, "purchases", options)
+    }
 
+    private async admin(req: Request, res: Response, next: NextFunction) {
+        this.title = "Administrator Access";
+        let userID = req.params.userID;
+
+        let allUsers = await this.requestAllUsers(req, res, next);
+
+        let options: Object = {
+            user: userID,
+            allUsers: allUsers
+        };
+        this.render(req, res, "admin", options)
     }
 }
--- tests/sampleFunctions.test.ts
+++
@@ -1,5 +0,0 @@
-import {sum} from './sampleFunctions';
-
-test('sample', () => {
-    expect(sum(1,2)).toBe(3);
-});
