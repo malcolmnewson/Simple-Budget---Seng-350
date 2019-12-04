7f0ef97d6cc84cfcbb143cc34ebffaef71ec5db1
Oliver Lewis <oliverlewis@uvic.ca>
2019-12-04 05:45:09 -0800

    merged

---
+++ docs/M5/README.md
@@ -0,0 +1,21 @@
+# Milestone 5 Demo
+### [User Stories](https://github.com/seng350/seng350f19-project-2-1/issues?utf8=%E2%9C%93&q=is%3Aissue+label%3A%22user+story%22+)
+
+1. As the Administrator, I want to be able to **create a user account** in order to manage accounts in the system.
+2. As the Administrator, I want to be able to **delete a user account** in order to manage accounts in the system.
+3. As a user, I want to **log into my account** in order to access my data.
+4. As a user, I want to **enter a purchase** into a spending category in order to track how much money has been spent in that category.
+5. As a user, I want to **see my spending history** within each category in order to see all past purchases. 
+6. As a user, I want to **edit previously logged purchases** in order to update them.
+7. As a user, I want to access a **summary of my expenses** in a variety of formats and time frames. 
+
+
+### [QAS](https://github.com/seng350/seng350f19-project-2-1/issues?q=is%3Aissue+label%3Aqas)
+
+1. **Usability** - Input validation on purchase amount.
+2. **Usability** - User can change the time frame of the summary and see the results within 1 second.
+3. **Usability** - Mulitple transactions can be submitted to different categories without leaving the page.
+4. **Performance** - Database should handle >10 users each with >1000 transations without decreasing the response time of the database queries by 1 second.
+5. **Performance** - A user's page loads within 1 second of login.
+6. **Maintainability** - Modifying the summary format should take <5 hours of development time.
+7. **Testability** - Ability to verify the information displayed is correctly linked to the account selected
---
+++ src/controllers/index.ts
@@ -0,0 +1,85 @@
+import {NextFunction, Request, Response, Router} from "express";
+import {BaseRoute} from "../routes/route";
+import {LoginController} from "./loginController";
+import {RequestData} from "../routes/requestData";
+import {PurchaseDao} from "../daos/purchaseDao";
+import {SummaryRoute} from "./summaryController";
+
+/**
+ * / route
+ *
+ * @class IndexRoute
+ */
+export class IndexRoute extends BaseRoute {
+
+    /**
+     * Create the routes.
+     *
+     * @class IndexRoute
+     * @method create
+     * @static
+     */
+    public static create(router: Router) {
+        // log
+        // console.log("[IndexRoute::create] Creating index route.");
+
+        // add home page route
+        router.get("/", async (req: Request, res: Response, next: NextFunction) => {
+            new IndexRoute().index(req, res, next);
+        });
+
+        router.get("/user/:userID", async (req: Request, res: Response) => {
+            new LoginController().login(req, res);
+        });
+
+        // add summary page route for different time frame
+        router.post("/user/summary/timeFrame", async (req: Request, res: Response, next: NextFunction) => {
+            req.params.userID = req.body.userID;
+            await new SummaryRoute().summary(req, res);
+        });
+        // add summary page route form purchases page
+        router.get("/user/summary/:userID", async (req: Request, res: Response, next: NextFunction) => {
+            await new SummaryRoute().summary(req, res);
+        });
+    }
+
+    /**
+     * Constructor
+     *
+     * @class IndexRoute
+     * @constructor
+     */
+    constructor() {
+        super();
+    }
+
+    /**
+     * The home page route.
+     *
+     * @class IndexRoute
+     * @method index
+     * @param req {Request} The express Request object.
+     * @param res {Response} The express Response object.
+     * @param next
+     * @next {NextFunction} Execute the next method.
+     */
+    public async index(req: Request, res: Response, next: NextFunction) {
+        // set custom title
+        this.title = "Budget App | Welcome!";
+
+        const userData = await new RequestData().requestAllUsers(res);
+        const userIDs = [];
+        for (const user of userData) {
+            userIDs.push(user.userID);
+        }
+
+        // set options
+        const options: object = {
+            instructions: "Select your user ID to view your purchasePage",
+            users: userIDs,
+        };
+
+        // render template
+        this.render(req, res, "index", options);
+    }
+}
